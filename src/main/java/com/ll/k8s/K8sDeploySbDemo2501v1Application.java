3b73734013dc1deaf6fb35d09ba3250795592652
sik2 <bickwang123@gmail.com>
2025-01-14 04:46:35 +0900

    init

---
+++ src/main/java/com/ll/k8s/K8sDeploySbDemo2501v1Application.java
@@ -0,0 +1,13 @@
+package com.ll.k8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8sDeploySbDemo2501v1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(K8sDeploySbDemo2501v1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/k8s/K8sDeploySbDemo2501v1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.k8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8sDeploySbDemo2501v1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
