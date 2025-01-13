ad1565369ae790435cefcdbeda2faaf4a695bfb4
sik2 <bickwang123@gmail.com>
2025-01-14 05:09:51 +0900

    work

--- src/test/java/com/ll/k8s/K8sDeploySbDemo2501v1ApplicationTests.java
+++ src/test/java/com/ll/k8s/K8sDeploySbDemo2501v1ApplicationTests.java
@@ -2,8 +2,10 @@ package com.ll.k8s;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("Test")
 class K8sDeploySbDemo2501v1ApplicationTests {
 
     @Test
